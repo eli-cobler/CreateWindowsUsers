df4f9aba21492a4cddfbb28e3e67c6b6a37c9100
Eli Cobler <ecobler11@gmail.com>
2018-10-02 22:23:45 -0500

    Auto Path Detections
    Now the batch file auto detects the current directory instead of it being hard coded in, making for easier user on multiple machines.

--- createuser.bat
+++ createuser.bat
@@ -1,3 +1,2 @@
-C:\Users\coblere\Documents\GitHub\CreateWindowsUsers\createusers\Scripts\activate
-python C:\Users\coblere\Documents\GitHub\CreateWindowsUsers\createUsers.py 
+python %cd%\createUsers.py 
 pause
